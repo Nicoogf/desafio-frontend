e802af08739be61c927d365984cd330267dc16c0
Nicolas <nicolasgfalabella@gmail.com>
2024-07-29 14:56:15 -0300

    Sprint 1: Api /Register y Creación de Token

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,8 +1,72 @@
+import User from "@/models/User";
 import { MongoDBConnection } from "@/utils/mongodb";
 import { NextResponse } from "next/server";
+import bycrypt from "bcryptjs";
+import { validarEmail } from "@/utils/funcionalidades";
+import { createAccesToken } from "@/utils/jwt";
+import { cookies } from "next/headers";
 
-export async function POST ( request ) {
- MongoDBConnection()
- const body = await request.json( request )
- return NextResponse.json( body )
+export async function POST(request) {
+    MongoDBConnection()
+    try {
+    const { email , password } = await request.json()
+
+    if(!email || !password){
+        return NextResponse.json(["Faltan ingresar credenciales"] , {status:400})
+    }
+    if (!validarEmail(email)) {
+        return NextResponse.json(["El email ingresado no es valido"], { status: 400 })
+      }
+    
+    const userFound = await User.findOne({email})
+
+    console.log(userFound)
+
+    if(!userFound){
+        return NextResponse.json(["El usuario no se encuentra registrado"] , {status:400})
+    }
+
+    const isMatch = await bycrypt.compare( password ,userFound.password )
+
+    if( !isMatch) {
+        return NextResponse.json(["La contraseña es invalida"] , {status : 400 })
+    }
+
+    console.log(isMatch)
+
+    const token = await createAccesToken({ id: userFound._id });
+
+    console.log(token)
+
+      // Establecer la cookie
+      cookies().set("token", token, {
+        // httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60 * 24, // 1 day
+        path: '/'   
+      }); 
+
+      console.log(userFound)
+
+      return NextResponse.json({
+        id: userFound._id,
+        name: userFound.name,
+        lastname: userFound.lastname,
+        email: userFound.email,
+        dni: userFound.dni,
+        rol: userFound.rol,
+        dinero: userFound.dinero,
+        phone: userFound.phone,
+        cbu: userFound.cbu,
+        alias: userFound.alias,
+        createdAt: userFound.createdAt,
+        updatedAt: userFound.updatedAt,
+        companyName: userFound.companyName,
+        businessField: userFound.businessField,
+        cuit: userFound.cuit
+      })       
+  
+    } catch (error) {
+        return NextResponse.json(error)
+    }
 }
--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,82 +1,106 @@
-import User from "@/models/User";
-import { validarEmail } from "@/utils/funcionalidades";
 import { MongoDBConnection } from "@/utils/mongodb";
+import { createAccesToken } from "@/utils/jwt";
+import { validarEmail } from "@/utils/funcionalidades";
 import { NextResponse } from "next/server";
-import bycrypt from "bcryptjs" ;
+import { cookies } from "next/headers";
+import bycrypt from "bcryptjs";
+import User from "@/models/User";
+
 
 export async function POST(request) {
-    MongoDBConnection()
+  MongoDBConnection()
+
+  try {
     const {
+      name,
+      lastname,
+      email,
+      dni,
+      rol,
+      phone,
+      dinero,
+      password,
+      companyName,
+      businessField,
+      cuit,
+      confirmPassword
+    } = await request.json()
+
+    const hashedPass = await bycrypt.hash(password, 12)
+
+    const newUserRegister = new User(
+      {
         name,
         lastname,
         email,
         dni,
         rol,
-        phone,
         dinero,
-        password,
+        phone,
+        password: hashedPass,
         companyName,
         businessField,
-        cuit,
-        confirmPassword
-    } = await request.json()
-
-    const hashedPass = await bycrypt.hash( password , 12 )
-    console.log(hashedPass)
-
-    const newUserRegister = new User(
-        {
-            name,
-            lastname,
-            email,
-            dni,
-            rol,
-            dinero,
-            phone,
-            password : hashedPass ,
-            companyName,
-            businessField,
-            cuit: `20${cuit}7`,
-        }
+        cuit: `20${cuit}7`,
+      }
     )
 
     if (newUserRegister.rol === "usuario") {
-        if (!name || !lastname || !email || !dni || !rol || !phone || !password) {
-            return NextResponse.json(["Faltan completar Campos"], { status: 400 })
-        }
+      if (!name || !lastname || !email || !dni || !rol || !phone || !password) {
+        return NextResponse.json(["Faltan completar Campos"], { status: 400 })
+      }
     }
 
     if (newUserRegister.rol === "empresa") {
-        if (!companyName || !businessField || !email || !cuit || !rol || !phone || !password) {
-            return NextResponse.json(["Faltan completar Campos"], { status: 400 })
-        }
+      if (!companyName || !businessField || !email || !cuit || !rol || !phone || !password) {
+        return NextResponse.json(["Faltan completar Campos"], { status: 400 })
+      }
     }
 
-    if( password !== confirmPassword) {
-        return NextResponse.json(["Las contraseñas ingresadas no coinciden"] , {status : 400})
+    const userFound = await User.findOne({ email })
+
+    if (userFound) {
+      return NextResponse.json(["El usuario ya se encuentra registrado"], { status: 400 })
     }
 
-    if( !validarEmail(email) ){
-        return NextResponse.json(["El email ingresado no es valido"] , {status : 400})
+    if (password !== confirmPassword) {
+      return NextResponse.json(["Las contraseñas ingresadas no coinciden"], { status: 400 })
     }
+
+    if (!validarEmail(email)) {
+      return NextResponse.json(["El email ingresado no es valido"], { status: 400 })
+    }
+
+    const token = await createAccesToken({ id: newUserRegister._id });
+
+    cookies().set("token", token, {
+      // httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 60 * 60 * 24, // 1 day
+      path: '/'
+    });
 
     newUserRegister.save()
 
     return NextResponse.json({
-        id: newUserRegister._id ,
-        name: newUserRegister.name,
-        lastname : newUserRegister.lastname,
-        email: newUserRegister.email,
-        dni: newUserRegister.dni,
-        rol: newUserRegister.rol,
-        dinero: newUserRegister.dinero,
-        phone: newUserRegister.phone,
-        cbu: newUserRegister.cbu,
-        alias: newUserRegister.alias,
-        createdAt: newUserRegister.createdAt,
-        updatedAt: newUserRegister.updatedAt,
-        companyName: newUserRegister.companyName,
-        businessField :newUserRegister.businessField,
-        cuit :newUserRegister.cuit
-    })
+      id: newUserRegister._id,
+      name: newUserRegister.name,
+      lastname: newUserRegister.lastname,
+      email: newUserRegister.email,
+      dni: newUserRegister.dni,
+      rol: newUserRegister.rol,
+      dinero: newUserRegister.dinero,
+      phone: newUserRegister.phone,
+      cbu: newUserRegister.cbu,
+      alias: newUserRegister.alias,
+      createdAt: newUserRegister.createdAt,
+      updatedAt: newUserRegister.updatedAt,
+      companyName: newUserRegister.companyName,
+      businessField: newUserRegister.businessField,
+      cuit: newUserRegister.cuit
+    },{status : 200 })
+    
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error }, { status: 400 })
+  }
 }
